37aad7441d3ce644a4bdc37b6c279dc3ed769b29
veko <vekonyat@gmail.com>
2024-10-02 23:08:22 +0200

    Teherauto osztály kész

--- autokolcsonzo.py
+++ autokolcsonzo.py
@@ -20,5 +20,17 @@ class Szemelyauto(Auto):
     def __str__(self):
         return f"Személyautó: {self.rendszam}, {self.tipus}, {self.berleti_dij}, {self.utasszam}"
 
+class Teherauto(Auto):
+    def __init__(self, rendszam, tipus, berleti_dij, terheles):
+        super().__init__(rendszam, tipus, berleti_dij)
+        self.terheles = terheles
+
+    def __str__(self):
+        return f"Teherautó: {self.rendszam}, {self.tipus}, {self.berleti_dij}, {self.terheles}"
+
 auto1 = Szemelyauto("ABC-123", "Toyota Corolla", 5000, 5)
-print(auto1)
+auto2 = Szemelyauto("ABC-124", "Honda Civic", 3000, 4)
+auto3 = Teherauto("ABC-125", "Ford Transit", 8000, 10)
+
+print(auto1, auto2, auto3)
+
