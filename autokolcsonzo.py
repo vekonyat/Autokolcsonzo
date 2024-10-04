ce19b7e49f5135f194ad858196a1ffe70e77f852
veko <vekonyat@gmail.com>
2024-10-05 00:15:16 +0200

    Autokolcsonzo elkezdve, autók hozzáadva

--- autokolcsonzo.py
+++ autokolcsonzo.py
@@ -28,9 +28,36 @@ class Teherauto(Auto):
     def __str__(self):
         return f"Teherautó: {self.rendszam}, {self.tipus}, {self.berleti_dij}, {self.terheles}"
 
+class Berles:
+    def __init__(self, auto, datum, ugyfel):
+        self.auto = auto
+        self.datum = datum
+        self.ugyfel = ugyfel
+
+    def __str__(self):
+        return f"Bérlés: {self.auto.rendszam} - {self.auto.tipus}, Dátum: {self.datum}, Ügyfél: {self.ugyfel}"
+
+class Autokolcsonzo:
+    def __init__(self, nev):
+        self.nev = nev
+        self.autok = []
+        self.berlesek = []
+
+    def auto_hozzaad(self, auto):
+        self.autok.append(auto)
+    def __str__(self):
+        return f"{self.nev} car rent - Rent a car!"
+
+kolcsonzo = Autokolcsonzo("Renter")
+
 auto1 = Szemelyauto("ABC-123", "Toyota Corolla", 5000, 5)
 auto2 = Szemelyauto("ABC-124", "Honda Civic", 3000, 4)
 auto3 = Teherauto("ABC-125", "Ford Transit", 8000, 10)
 
+kolcsonzo.auto_hozzaad(auto1)
+kolcsonzo.auto_hozzaad(auto2)
+kolcsonzo.auto_hozzaad(auto3)
+
 print(auto1, auto2, auto3)
+print(kolcsonzo, kolcsonzo.autok)
 
